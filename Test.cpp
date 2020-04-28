6f499de6e987e38e091fcd70defc27735cc671f6
Noa <noa.aizer@gmail.com>
2020-04-28 12:55:02 +0300

    upload part B

--- Test.cpp
+++ Test.cpp
@@ -30,17 +30,17 @@ TEST_CASE("Real Variables - Linear"){
     CHECK(solve((6*x)/2 == 12) == 4);
     CHECK(solve((18)/2 == 3*x) == 3);
     CHECK(solve((2*x+2) == 3*x+2) == 0);
-    CHECK(solve((2*x+2) == 4*x-14) == 6);
+    CHECK(solve((2*x+2) == 4*x-14) == 8);
     CHECK(solve((x+2)/2 == 3) == 4);
     CHECK(solve((x/2)*2 == 2*x+7) == -7);
     CHECK(solve((x*2) == 3) == 1.5);
-    CHECK(solve((4*x) == 10) == 0.4);
-    CHECK(solve((2*x)-1 == 0) == 1.5);
+    CHECK(solve((4*x) == 10) == 2.5);
+    CHECK(solve((2*x)-1 == 0) == 0.5);
     CHECK_THROWS_AS(solve((2*x)/0 ==1),std::exception);
     CHECK_THROWS_AS(solve(4*x -(2*x)*2 ==1),std::exception);
     CHECK_THROWS_AS(solve((2*x) ==x/0) ,std::exception);
     CHECK_THROWS_AS(solve((2*x)/(4-3-1) ==1),std::exception);
-    CHECK_THROWS_AS(solve(1*x == 2/(-1+1)),std::exception);
+    CHECK_THROWS_AS(solve(1*x == (2*x)/(2-1-1)),std::exception);
     
 
 }
@@ -49,29 +49,29 @@ TEST_CASE("Real Variables - polynom"){
     
     RealVariable x;
     
-    CHECK(((solve(x^2 == 4) == 2 )|| (solve(x^2 == 4) == -2)));
-    CHECK(((solve((x^2)== -1*x) == -1 )|| (solve((x^2 )== -1*x) == 0) ));
-    CHECK(((solve(x^2-25 == 0) == 5) || (solve(x^2-25 == 0) == -5)));
+    CHECK(((solve((x^2) == 4) == 2 )|| (solve((x^2) == 4) == -2)));
+    CHECK(((solve((x^2)== -1*x) == -1 )|| (solve((x^2)== -1*x) == 0) ));
+    CHECK(((solve((x^2)-25 == 0) == 5) || (solve((x^2)-25 == 0) == -5)));
     CHECK(((solve(2*(x^2)+10*x+8 == 0) == -1 )|| (solve(2*(x^2)+10*x+8 == 0) == -4 )));
-    CHECK(((solve(x^2==16) == 4 )|| (solve(x^2==16) == -4)));
-    CHECK(((solve(81 == x^2) == 9 )|| (solve(81 == x^2) == -9)));
-    CHECK(((solve(0 == x^2-49) == 7 )|| (solve(0 == x^2-49) == -7)));
-    CHECK(((solve(2*(x^2) == 8*2) == 2 )|| (solve(2*(x^2) == 8*2) == -2)));
-    CHECK(solve(0 == 1+ 2*(x^4) == 0));
-    CHECK(((solve(-1*((x^2)+8*x+20 == 0) == 2) || (solve(-1*((x^2)+8*x+20 == 0) == -10)))));
-    CHECK(((solve(0*(x^2) + (x^2  == 16) == 4 )|| (solve(0*(x^2) + (x^2  == 16) == -4 )))));
-    CHECK(solve((2*x^2)/2 +2*x + 1 == 0) == -1);
+    CHECK(((solve((x^2)==16) == 4 )|| (solve((x^2)==16) == -4)));
+    CHECK(((solve(81 == (x^2)) == 9 )|| (solve(81 == (x^2)) == -9)));
+    CHECK(((solve(0 == (x^2)-49) == 7 )|| (solve(0 ==(x^2)-49) == -7)));
+    CHECK(((solve(2*(x^2) == 8) == 2 )|| (solve(2*(x^2) == 8) == -2)));
+    CHECK(solve(0 == 1+ 2*(x^2) == 0));
+    CHECK(((solve((-1)*(x^2)+8*x+20 == 0) == -2) || (solve((-1)*(x^2)+8*x+20 == 0) == 10)));
+    CHECK(((solve(0*(x^2) + ((x^2)  == 16) == 4 )|| (solve(0*(x^2) + ((x^2)  == 16) == -4 )))));
+    CHECK(solve(((x^2)+2*x == -1)) == -1);
     CHECK(((solve((x^2)+x/2 == 2-x/2  ) == -2 )|| (solve((x^2)+x/2 == 2-x/2  ) == 1 )));
-    CHECK(((solve((x^2)+6*x+8 == x +2 ) == 1) || (solve((x^2)+6*x+8 == x +2 ) == 6)));
-    CHECK(((solve((x^2)+5*x+6 == x +6 ) == 2) ||(solve((x^2)+5*x+6 == x +6 ) == -4)));
+    CHECK(((solve((x^2)+6*x+8 == x +2 ) == -3) || (solve((x^2)+6*x+8 == x +2 ) == -2)));
+    CHECK(((solve((x^2)+5*x+6 == x +6 ) == 0) ||(solve((x^2)+5*x+6 == x +6 ) == 4)));
     CHECK(((solve((x^2) == 8-(x^2) ) == 2) || (solve((x^2) == 8-(x^2 )) == -2)));
-    CHECK(((solve(2*(x^2)+5*x-3 == (x^2)+6*x+3 ) == 1) || (solve(2*(x^2)+5*x-3 == (x^2)+6*x+3 ) == 0)));
+    CHECK(((solve(2*(x^2)+5*x-3 == (x^2)+6*x+3 ) == 3) || (solve(2*(x^2)+5*x-3 == (x^2)+6*x+3 ) == -2)));
     CHECK(((solve(3*(x^2)-8*x-4 == (x^2)-3*x-6 ) == 2) ||(solve(3*(x^2)-8*x-4 == (x^2)-3*x-6 ) == 0.5)));
     CHECK(((solve((x^2)+3*x-4 == 3-(x^2)-2*x ) == -3.5) || (solve((x^2)+3*x-4 == 3-(x^2)-2*x ) == 1)));
     CHECK(((solve(5*(x^2)+x+2 == (x^2)+x+6 ) == 1) || (solve(5*(x^2)+x+2 == (x^2)+x+6 ) == -1)));
     CHECK(((solve(3*(x^2)+6 == -2*(x^2)-7*x+4 ) == -1) || (solve(3*(x^2)+6 == -2*(x^2)-7*x+4 ) == -0.4)));
     CHECK(((solve(2*(x^2)+4*x-5 == (x^2)+2*x+3 ) == 2) || (solve(2*(x^2)+4*x-5 == (x^2)+2*x+3 ) == -4)));
-    CHECK(((solve((x^2)+x-12 == (x^2)+2*x+3 ) == 3) || (solve((x^2)+x-12 == (x^2)+2*x+3 ) == -2.5)));
+    CHECK(solve((x^2)+x-12 == (x^2)+2*x+3 ) == -15);
     CHECK(((solve((x^2)+10*x+25 == -25-(x^2)-10*x ) == -5)));
     CHECK(((solve(3*(x^2)+14*x+1 == 2*(x^2)+8*x-8 ) == -3)));
     CHECK_THROWS_AS(solve((x^5)/0 ==1),std::exception);
@@ -95,7 +95,7 @@ TEST_CASE("Complex Variables - Linear"){
     CHECK(solve(2*y-4 == 4) == complex(4.0,0.0));
     CHECK(solve(2*y-6 == 4.0) == complex(5.0,0.0));
     CHECK(solve(y + 4 == 4) == complex(0.0,0.0));
-    CHECK(solve(2*y == 3-2*y) == complex(0.75,0.0));
+    CHECK(solve(2*y == 3+(-2)*y) == complex(0.75,0.0));
     CHECK(solve(4 == 2*y) == complex(2.0,0.0));
     CHECK(solve(y/2 == 4) == complex(8.0,0.0));
     CHECK(solve(2*y+(2*y)*2 == (4+4)*2+2) == complex(3.0,0.0));
@@ -119,24 +119,24 @@ TEST_CASE("Complex Variables - Polynom"){
     
     ComplexVariable y;
 
-    CHECK(((solve(y^2 == 4) == complex(2.0,0.0))|| (solve(y^2 == 4) == complex(-2.0,0.0))));
-    CHECK(solve((y^2)+y == 0) == complex(0.0,0.0));
+    CHECK(((solve((y^2) == 4) == complex(2.0,0.0))|| (solve((y^2) == 4) == complex(-2.0,0.0))));
+    CHECK(((solve((y^2)+2*y +3 == 0) == complex(-1.0,sqrt(2)))|| (solve((y^2)+2*y +3 == 0) == complex(-1.0,-sqrt(2)))));
     CHECK(((solve((y^2)+y == 0) == complex(-1.0,0.0) )|| (solve((y^2)+y == 0) == complex(0.0,0.0)) ));
-    CHECK(((solve(y^2 + 25 == 0) == complex(0.0,5.0)) || (solve(y^2 + 25 == 0) == complex(0.0,-5.0))));
+    CHECK(((solve((y^2) + 25 == 0) == complex(0.0,5.0)) || (solve((y^2) + 25 == 0) == complex(0.0,-5.0))));
     CHECK(((solve(2*(y^2)-8*y == complex(10.0,-20.0) + complex(0.0,12.0)*y) == complex(1.0,2.0) )||
      (solve(2*(y^2)-8*y == complex(10.0,-20.0) + complex(0.0,12.0)*y) == complex(3.0,4.0) )));
     CHECK(((solve((y^2) +4*y + 7 ==0 ) == complex(-2.0, -sqrt(3)))|| (solve((y^2) +4*y + 7 ==0 ) == complex(-2.0, sqrt(3)))));
-    CHECK(((solve(-81 == y^2) == complex(0.0,9.0) )|| (solve(81 == y^2) == complex(0.0,-9.0))));
+    CHECK(((solve(-81 == (y^2)) == complex(0.0,9.0) )|| (solve(81 == (y^2)) == complex(0.0,-9.0))));
     CHECK(((solve(6*(y^2)/2 + 6*y + 3 == 3) == complex(0.0,0.0) )|| (solve(6*(y^2)/2 + 6*y + 3 == 3) == complex(-2.0,0.0) )));
     CHECK(((solve(3*(y^2)+6 == -2*(y^2)-7*y+4 ) == complex(-1.0,0.0)) || solve(3*(y^2)+6 == -2*(y^2)-7*y+4 ) == complex(-0.4,0.0)));
     CHECK(((solve(2*(y^2)+4*y-5 == (y^2)+2*y+3 ) == complex(2.0,0.0)) || solve(2*(y^2)+4*y-5 == (y^2)+(2*y)+3 ) == complex(-4.0,0.0)));
-    CHECK(((solve((y^2)+y-12 == (y^2)+2*y+3 ) == complex(3.0,0.0)) || solve((y^2)+y-12 == (y^2)+2*y+3 ) == complex(-2.5,0.0)));
-    CHECK(((solve(y^2 == -16) == complex(0.0,4.0) )|| (solve(y^2 == -16) == complex(0.0,-4.0)) ));
-    CHECK(((solve((y^2)*2 == -19) == complex(0.0,3.0) )|| (solve((y^2)*2 == -19) == complex(0.0,-3.0)) ));
-    CHECK_THROWS_AS(solve((2*y^2)/0 ==1), std::exception);
-    CHECK_THROWS_AS(solve((2*y^3) == y/0), std::exception);
-    CHECK_THROWS_AS(solve((2*y^3) == y), std::exception);
-    CHECK_THROWS_AS(solve((2*y) == y^5), std::exception);
-    CHECK_THROWS_AS(solve((2*y^2)/(4-3-1) == 1), std::exception);  
+    CHECK(solve((y^2)+y-12 == (y^2)+2*y+3 ) == complex(-15.0,0.0));
+    CHECK(((solve((y^2) == -16) == complex(0.0,4.0) )|| (solve((y^2) == -16) == complex(0.0,-4.0)) ));
+    CHECK(((solve((y^2)*2 == -8) == complex(0.0,2.0) )|| (solve((y^2)*2 == -8) == complex(0.0,-2.0)) ));
+    CHECK_THROWS_AS(solve((2*(y^2))/0 ==1), std::exception);
+    CHECK_THROWS_AS(solve((2*(y^3)) == y/0), std::exception);
+    CHECK_THROWS_AS(solve((2*(y^3)) == y), std::exception);
+    CHECK_THROWS_AS(solve((2*y) == (y^5)), std::exception);
+    CHECK_THROWS_AS(solve((2*(y^2))/(4-3-1) == 1), std::exception);  
 
 }
--- solver.cpp
+++ solver.cpp
@@ -7,153 +7,198 @@ using namespace std ;
 namespace solver{
      // + operator
      RealVariable operator+(const RealVariable& n1, const RealVariable &n2 ){
-        cout << "func" << endl;
-        return RealVariable(1,1,1);
+          return RealVariable(n1.a+n2.a,n1.b+n2.b,n1.c+n2.c);
     }
      RealVariable operator+(const RealVariable& n1, const double n2 ){
-          cout << "func" << endl;
-        return RealVariable(1,1,1);
+          return RealVariable(n1.a,n1.b,n1.c+n2);
     }
      RealVariable operator+(const double n1, const RealVariable& n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+          return RealVariable(n2.a,n2.b,n1+n2.c);
     }
 
     // - operator
      RealVariable operator-(const RealVariable& n1,const RealVariable& n2 ){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+           return RealVariable(n1.a-n2.a,n1.b-n2.b,n1.c-n2.c);
     }
      RealVariable operator-(const RealVariable& n1, const double n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+        return RealVariable(n1.a,n1.b,n1.c-n2);
     }
      RealVariable operator-(const double n1, const RealVariable& n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+        return RealVariable((-1)*n2.a,(-1)*n2.b,n1-n2.c);
     }
 
     //* operator
      RealVariable operator*(const RealVariable& n1, const RealVariable &n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+    return RealVariable  ((n1.a*n2.c+n1.c*n2.a+n1.b*n2.b),
+                         (n1.c*n2.b+n1.b*n2.c),
+                         (n1.c*n2.c));
+
+                       
+                         
     }
      RealVariable operator*(const RealVariable& n1, const double n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+        return RealVariable(n1.a*n2,n1.b*n2,n1.c*n2);
     }
      RealVariable operator*(const double n1, const RealVariable& n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+          return RealVariable(n1*n2.a,n1*n2.b,n1*n2.c);
     }
 
     //: operator
     RealVariable operator/(const RealVariable& n1, const RealVariable &n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+         if((n2.a==0)&&(n2.b==0)&& (n2.c==0))
+          throw runtime_error {"Divison by zero is invalid "};
+          return RealVariable(n1.a/n2.a,n1.b/n2.b,n1.c/n2.c);
+
     }
     RealVariable operator/(const RealVariable& n1, const double n2){
-         cout << "func" << endl;
-       return RealVariable(1,1,1);
+          if(n2==0)
+          throw runtime_error {"Divison by zero is invalid "};
+
+       return RealVariable(n1.a/n2,n1.b/n2,n1.c/n2);
     }
     RealVariable operator/(const double n1, const RealVariable& n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+          if((n2.a==0)&&(n2.b==0)&& (n2.c==0))
+          throw runtime_error {"Divison by zero is invalid "};
+
+          return RealVariable(n1/n2.a,n1/n2.b,n1/n2.c);
     }
 
     // ^ operator
     RealVariable operator^(const RealVariable &n1, const double n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+         //cout << "a:" <<n1.a <<  "b:" << n1.b << "c: "<<n1.c << "n2: "<<n2<< endl;
+         if(n2 ==0) return RealVariable(0,0,1);
+         if(n2 ==1) return n1;
+         if(n2 ==2 && n1.a==0 && n1.b!=0 && n1.c!=0) return RealVariable(1,n1.b*n2*n1.c,n1.c*n1.c);
+         if(n2 ==2 && n1.a==0 && n1.b==0 && n1.c!=0) return RealVariable(0,0,n1.c*n1.c);
+         if(n2 ==2 && n1.a==0 && n1.b!=0 && n1.c==0) return RealVariable(n1.b,0,0);
+         if(n2 >2 && n1.a==0 && n1.b==0 && n1.c!=0) return RealVariable(0,0,pow(n1.c,n2));
+
+          throw runtime_error {"Invalid power "};
+
         }
 
     // == operator
     RealVariable operator==(const RealVariable& n1, const RealVariable& n2){ 
-        cout << "func" << endl;
-        return RealVariable(1,1,1);}
+         return n1-n2;
+    }
     RealVariable operator==(const RealVariable& n1, const double n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+         return n1-n2;
     }
     RealVariable operator==(const double n1, const RealVariable& n2){
-         cout << "func" << endl;
-        return RealVariable(1,1,1);
+         return n2-n1;
     }
 
       // + operator
         ComplexVariable operator+(const ComplexVariable& n1,const ComplexVariable &n2 ){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+             return ComplexVariable(n1.a+n2.a,n1.b+n2.b,n1.c+n2.c);
         }
         ComplexVariable operator+(const ComplexVariable& n1, const complex<double> n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+             return ComplexVariable(n1.a,n1.b,n1.c+n2);
         }
         ComplexVariable operator+(const complex<double> n1,const ComplexVariable& n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+             return ComplexVariable(n2.a,n2.b,n1+n2.c);
         }
         // - operator
         ComplexVariable operator-(const ComplexVariable& n1,const ComplexVariable &n2 ){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+             return ComplexVariable(n1.a-n2.a,n1.b-n2.b,n1.c-n2.c);
         }
         ComplexVariable operator-(const ComplexVariable& n1,const complex<double> n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+              return ComplexVariable(n1.a,n1.b,n1.c-n2);
         }
         ComplexVariable operator-(const complex<double> n1,const ComplexVariable& n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+              return ComplexVariable(n2.a,n2.b,n1-n2.c);
         }
         //* operator
         ComplexVariable operator*(const ComplexVariable& n1, const ComplexVariable &n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+               return ComplexVariable  ((n1.a*n2.c+n1.c*n2.a+n1.b*n2.b),
+                                        (n1.c*n2.b+n1.b*n2.c),
+                                        (n1.c*n2.c));
         }
         ComplexVariable operator*(const ComplexVariable& n1, const complex<double> n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+               return ComplexVariable(n1.a*n2,n1.b*n2,n1.c*n2);
         }
         ComplexVariable operator*(const complex<double> n1,const ComplexVariable& n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+               return ComplexVariable(n1*n2.a,n1*n2.b,n1*n2.c);
         }
         //: operator
-        ComplexVariable operator/(const ComplexVariable& n1, const ComplexVariable &yn2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+        ComplexVariable operator/(const ComplexVariable& n1, const ComplexVariable & n2){
+          if((n2.a==complex(0.0,0.0))&&(n2.b==complex(0.0,0.0))&& (n2.c==complex(0.0,0.0)))
+               throw runtime_error {"Divison by zero is invalid "};
+          return ComplexVariable(n1.a/n2.a,n1.b/n2.b,n1.c/n2.c);
         }
         ComplexVariable operator/(const ComplexVariable& n1, const complex<double> n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+           if(n2==complex(0.0,0.0))
+          throw runtime_error {"Divison by zero is invalid "};
+
+          return ComplexVariable(n1.a/n2,n1.b/n2,n1.c/n2);
         }
         
         // ^ operator
         ComplexVariable operator^(const ComplexVariable &n1, const complex<double> n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+         if(n2 ==complex<double>(0.0,0.0))
+          return ComplexVariable(0,0,1);
+         if(n2 ==complex<double>(1.0,0.0))
+          return n1;
+         if(n2 ==complex<double>(2.0,0.0) && n1.a==complex<double>(0.0,0.0) && n1.b!=complex<double>(0.0,0.0) && n1.c!=complex<double>(0.0,0.0))
+          return ComplexVariable(1,n1.b*n2*n1.c,n1.c*n1.c);
+         if(n2 ==complex<double>(2.0,0.0) && n1.a==complex<double>(0.0,0.0) && n1.b==complex<double>(0.0,0.0) && n1.c!=complex<double>(0.0,0.0)) 
+          return ComplexVariable(0,0,n1.c*n1.c);
+         if(n2 ==complex<double>(2.0,0.0) && n1.a==complex<double>(0.0,0.0) && n1.b!=complex<double>(0.0,0.0) && n1.c==complex<double>(0.0,0.0)) 
+          return ComplexVariable(n1.b,0,0);
+         if(n2.real() >2 && n1.a==complex<double>(0.0,0.0) && n1.b==complex<double>(0.0,0.0) && n1.c!=complex<double>(0.0,0.0)) 
+          return ComplexVariable(0,0,pow(n1.c,n2));
+
+          throw runtime_error {"Invalid power "};
         }
         // == operator
         ComplexVariable operator==(const ComplexVariable &n1, const ComplexVariable &n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+             return n1-n2;
         }
         ComplexVariable operator==(const ComplexVariable &n1, const complex<double> n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+             return n1-n2;
         }
         ComplexVariable operator==(const complex<double> n1,const ComplexVariable &n2){
-             cout << "func" << endl;
-        return ComplexVariable(1,1,1);
+             return n2-n1;
         }
 
     double solve (const RealVariable& x){
-         cout << "func" << endl;
-        return 1.0;
+         double a = x.getA();
+         double b = x.getB();
+         double c = x.getC();
+         //cout << "a:" <<a <<  "b:" << b << "c: "<<c << endl;
+          if(a!=0 && b==0 && c!=0){
+               return sqrt(c/-a);
+          }
+          if(a==0 && b!=0){
+               return c/-b;
+          }
+          else if( a==0 && b==0 && c!=0){
+               throw runtime_error {"No solution! "};
+          }
+
+          double temp= b*b - 4*a*c;
+          if (temp >= 0) {
+               return (-b + sqrt(temp)) / (2*a);
+          }
+
+         else throw runtime_error {"There is no Real solution! "};
+
     }
+
     complex<double> solve (const ComplexVariable & x){
-         cout << "func" << endl;
-        return complex(1.0,1.0);
+         complex<double> a = x.getA();
+         complex<double> b = x.getB();
+         complex<double> c = x.getC();
+
+          if(a==complex<double>(0.0,0.0) && b!=complex<double>(0.0,0.0))
+               return c/-b;
+          if( a==complex<double>(0.0,0.0) && b==complex<double>(0.0,0.0) && c!=complex<double>(0.0,0.0)){
+               cout << "a:" <<a <<  "b:" << b << "c: "<<c << endl;
+               throw runtime_error {"No solution! "};}
+
+          complex<double> temp= b*b - complex(4.0,0.0)*a*c;
+          return ((-b+sqrt(temp))/(complex(2.0,0.0)*a));
     }
+          
+    
 }
